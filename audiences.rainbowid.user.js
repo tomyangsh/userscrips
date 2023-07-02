cadbfd414769eb7c0af18156e5ebcdddb3f55499
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-07-02 00:39:03 -0400

    add audiences.rainbowid

---
+++ audiences.rainbowid.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        咱要彩虹id
+// @namespace   https://github.com/tomyangsh/userscrips
+// @match       https://audiences.me/*
+// @version     1.0
+// @author      大統領
+// ==/UserScript==
+
+document.querySelectorAll('.Peasant_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.User_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.PowerUser_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.EliteUser_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.CrazyUser_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.InsaneUser_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.VeteranUser_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.ExtremeUser_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.UltimateUser_Name').forEach(node => node.className = 'Rainbow_Name');
+document.querySelectorAll('.NexusMaster_Name').forEach(node => node.className = 'Rainbow_Name');
