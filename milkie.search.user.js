6ee31aff31bea20cb97bfb88986455d9cabd6413
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-11-11 01:05:55 -0500

    milkie.search: add red

---
+++ milkie.search.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        milkie - search
+// @namespace   https://github.com/tomyangsh/userscrips
+// @match       https://milkie.cc/browse/*
+// @version     1.1
+// @author      大統領
+// ==/UserScript==
+
+setTimeout(function() {
+  var music_title_parse = document.querySelector('title').innerText.match(/([^-]+)-([^-]+)/);
+  var music_query = `${title_parse[1].replaceAll('_', ' ')} ${title_parse[2].replaceAll('_', ' ')}`
+  var ops_search_url = `https://orpheus.network/torrents.php?searchstr=${query}`;
+  var ops_search_link = document.querySelectorAll('div.inner')[1].appendChild(document.createElement("a"));
+  ops_search_link.innerText = 'search ops';
+  ops_search_link.href = ops_search_url;
+  ops_search_link.target = "_blank";
+  var red_search_url = `https://redacted.ch/torrents.php?searchstr=${query}`;
+  red_search_link.innerText = 'search red';
+  red_search_link.href = red_search_url;
+  red_search_link.target = "_blank";
+}, 1000);
