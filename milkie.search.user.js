3114a68681b66e6e7f8c85c28798dce1d9c301e7
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-11-11 01:09:20 -0500

    milkie.search: fix varibles

--- milkie.search.user.js
+++ milkie.search.user.js
@@ -2,19 +2,19 @@
 // @name        milkie - search
 // @namespace   https://github.com/tomyangsh/userscrips
 // @match       https://milkie.cc/browse/*
-// @version     1.1
+// @version     1.1.1
 // @author      大統領
 // ==/UserScript==
 
 setTimeout(function() {
   var music_title_parse = document.querySelector('title').innerText.match(/([^-]+)-([^-]+)/);
-  var music_query = `${title_parse[1].replaceAll('_', ' ')} ${title_parse[2].replaceAll('_', ' ')}`
-  var ops_search_url = `https://orpheus.network/torrents.php?searchstr=${query}`;
+  var music_query = `${music_title_parse[1].replaceAll('_', ' ')} ${music_title_parse[2].replaceAll('_', ' ')}`
+  var ops_search_url = `https://orpheus.network/torrents.php?searchstr=${music_query}`;
   var ops_search_link = document.querySelectorAll('div.inner')[1].appendChild(document.createElement("a"));
   ops_search_link.innerText = 'search ops';
   ops_search_link.href = ops_search_url;
   ops_search_link.target = "_blank";
-  var red_search_url = `https://redacted.ch/torrents.php?searchstr=${query}`;
+  var red_search_url = `https://redacted.ch/torrents.php?searchstr=${music_query}`;
   red_search_link.innerText = 'search red';
   red_search_link.href = red_search_url;
   red_search_link.target = "_blank";
