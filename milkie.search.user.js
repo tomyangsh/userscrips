ec91f064a2a7f97fe0ef2eb9fd10e40e52199005
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-11-13 13:00:26 -0500

    milkie.search: improve format

--- milkie.search.user.js
+++ milkie.search.user.js
@@ -2,20 +2,22 @@
 // @name        milkie - search
 // @namespace   https://github.com/tomyangsh/userscrips
 // @match       https://milkie.cc/browse/*
-// @version     1.1.2
+// @version     1.1.3
 // @author      大統領
 // ==/UserScript==
 
 setTimeout(function() {
-  var music_title_parse = document.querySelector('title').innerText.match(/([^-]+)-([^-]+)/);
-  var music_query = `${music_title_parse[1].replaceAll('_', ' ')} ${music_title_parse[2].replaceAll('_', ' ')}`
+  var link_box = document.querySelectorAll('div.inner')[1]
+  var music_title_parse = document.querySelector('title').innerText.match(/([^-]+)-([^-]+?)(_EP)?/);
+  var music_query = `${music_title_parse[2].replaceAll('_', ' ')}`
   var ops_search_url = `https://orpheus.network/torrents.php?searchstr=${music_query}`;
-  var ops_search_link = document.querySelectorAll('div.inner')[1].appendChild(document.createElement("a"));
+  var ops_search_link = link_box.appendChild(document.createElement("a"));
   ops_search_link.innerText = 'search ops';
   ops_search_link.href = ops_search_url;
   ops_search_link.target = "_blank";
+  link_box.appendChild(document.createElement("br"));
   var red_search_url = `https://redacted.ch/torrents.php?searchstr=${music_query}`;
-  var red_search_link = document.querySelectorAll('div.inner')[1].appendChild(document.createElement("a"));
+  var red_search_link = link_box.appendChild(document.createElement("a"));
   red_search_link.innerText = 'search red';
   red_search_link.href = red_search_url;
   red_search_link.target = "_blank";
