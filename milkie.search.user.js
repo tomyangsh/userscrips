3a369a70dc4def631c7d6fbf28e362077fe7fa26
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-12-17 14:41:50 -0500

    milkie.search: use MutationObserver to add search link only when info is loaded

--- milkie.search.user.js
+++ milkie.search.user.js
@@ -1,24 +1,25 @@
 // ==UserScript==
 // @name        milkie - search
 // @namespace   https://github.com/tomyangsh/userscrips
-// @match       https://milkie.cc/browse/*
-// @version     1.1.4
+// @match       https://milkie.cc/*
+// @version     1.2.0
 // @author      大統領
 // ==/UserScript==
 
-setTimeout(function() {
+function add_search(mutations) {
   var link_box = document.querySelectorAll('div.inner')[1]
-  var music_title_parse = document.querySelector('title').innerText.match(/([^-]+)-([^-]+?)(-|_EP)/);
-  var music_query = `${music_title_parse[2].replaceAll('_', ' ')}`
-  var ops_search_url = `https://orpheus.network/torrents.php?searchstr=${music_query}`;
-  var ops_search_link = link_box.appendChild(document.createElement("a"));
-  ops_search_link.innerText = 'search ops';
-  ops_search_link.href = ops_search_url;
-  ops_search_link.target = "_blank";
-  link_box.appendChild(document.createElement("br"));
-  var red_search_url = `https://redacted.ch/torrents.php?searchstr=${music_query}`;
-  var red_search_link = link_box.appendChild(document.createElement("a"));
-  red_search_link.innerText = 'search red';
-  red_search_link.href = red_search_url;
-  red_search_link.target = "_blank";
-}, 1000);
+  var music_title_parse = mutations[0].target.innerText.match(/([^-]+)-([^-]+?)(-|_EP)/);
+  if (music_title_parse) {
+    var music_query = `${music_title_parse[1].replaceAll('_', ' ')} ${music_title_parse[2].replaceAll('_', ' ')}`
+    var red_search_url = `https://redacted.ch/torrents.php?searchstr=${music_query}`;
+    var red_search_link = link_box.appendChild(document.createElement("a"));
+    red_search_link.innerText = 'search red';
+    red_search_link.href = red_search_url;
+    red_search_link.target = "_blank";
+  }
+};
+
+var target = document.querySelector('title');
+var observer = new MutationObserver(add_search);
+var config = { childList: true };
+observer.observe(target, config);
