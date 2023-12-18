3e26ad4fdb4c397a7bf97b716bb074742b04bb96
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-12-18 11:40:13 -0500

    milkie.search:improve link style; improve music title parse

--- milkie.search.user.js
+++ milkie.search.user.js
@@ -2,21 +2,26 @@
 // @name        milkie - search
 // @namespace   https://github.com/tomyangsh/userscrips
 // @match       https://milkie.cc/*
-// @version     1.2.0
+// @version     1.2.1
 // @author      大統領
 // ==/UserScript==
 
 function add_search(mutations) {
-  var link_box = document.querySelectorAll('div.inner')[1]
-  var music_title_parse = mutations[0].target.innerText.match(/([^-]+)-([^-]+?)(-|_EP)/);
+  var info_box = document.querySelectorAll('div.inner')[0];
+  var title = mutations[0].target.innerText;
+  if (title.match(/^(Browse|Details)/)) { return; };
+  var music_title_parse = title.match(/([^-]+)-([^-]+?)(-|_EP).+WEB-FLAC/);
   if (music_title_parse) {
     var music_query = `${music_title_parse[1].replaceAll('_', ' ')} ${music_title_parse[2].replaceAll('_', ' ')}`
-    var red_search_url = `https://redacted.ch/torrents.php?searchstr=${music_query}`;
-    var red_search_link = link_box.appendChild(document.createElement("a"));
-    red_search_link.innerText = 'search red';
-    red_search_link.href = red_search_url;
-    red_search_link.target = "_blank";
-  }
+    var search_url = `https://redacted.ch/torrents.php?searchstr=${music_query}`;
+  } else { return; }
+  var search_link = document.createElement("a");
+  search_link.innerText = 'Search RED';
+  search_link.href = search_url;
+  search_link.target = "_blank";
+  search_link.setAttribute('class', 'mat-caption');
+  search_link.setAttribute('style', 'color: #ffc107;');
+  info_box.append(search_link);
 };
 
 var target = document.querySelector('title');
