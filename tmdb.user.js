5148ff012eae525007e90616fd02437901fcea4d
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-12-21 18:13:46 -0500

    tmdb: remove next_episode due to provided by defualt

--- tmdb.user.js
+++ tmdb.user.js
@@ -5,78 +5,68 @@
 // @include     /^https://www\.themoviedb\.org/movie/[0-9a-z-]+$/
 // @include     /^https://www\.themoviedb\.org/tv/[0-9a-z-]+$/
 // @require     https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js
-// @version     1.6.3
+// @version     1.6.4
 // ==/UserScript==
 
-(function() {
-  'use strict'
+var social_links = document.querySelector("div.social_links");
 
-  var social_links = document.getElementsByClassName("social_links")[0];
+const m = location.pathname.match(/\/(\w+)\/(\d+)/);
+const cat = m[1];
+const id = m[2];
+const url = 'https://tomyangsh.pw/api/tmdb?cat=' + cat + '&id=' + id;
+const xhr  = new XMLHttpRequest();
+xhr.open("GET", url, true);
+xhr.setRequestHeader("User-Agent", document.baseURI);
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    let result = JSON.parse(xhr.responseText);
 
-  const m = location.pathname.match(/\/(\w+)\/(\d+)/);
-  const cat = m[1];
-  const id = m[2];
-  const url = 'https://tomyangsh.pw/api/tmdb?cat=' + cat + '&id=' + id;
-  const xhr  = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.setRequestHeader("User-Agent", document.baseURI);
-  xhr.onload = () => {
-    if (xhr.readyState === 4 & xhr.status === 200) {
-      let result = JSON.parse(xhr.responseText);
+    let zh_names = result.zh_names;
 
-      let zh_names = result.zh_names;
+    if (zh_names) {
+      var zh_names_span = document.createElement('span');
+      zh_names_span.innerText = zh_names;
+      zh_names_span.style = "font-size: 30px;";
+      document.querySelector('h2 a').replaceWith(zh_names_span);
+    }
 
-      if (zh_names) {
-        var zh_names_span = document.createElement('span');
-        zh_names_span.innerText = zh_names;
-        zh_names_span.style = "font-size: 30px;";
-        document.querySelector('h2 a').replaceWith(zh_names_span);
-      }
+    let imdb = result.imdb;
 
-      let imdb = result.imdb;
+    if (imdb) {
+      let imdb_link = social_links.appendChild(document.createElement("a"));
+      imdb_link.href = `https://www.imdb.com/title/${imdb}/`;
+      imdb_link.target = "_blank";
+      var icon = imdb_link.appendChild(document.createElement("img"));
+      icon.src = "https://tomyangsh.pw/imdb.svg";
+      icon.width = 30;
+      var trakt_link = social_links.appendChild(document.createElement("a"));
+      trakt_link.href = `https://trakt.tv/search/imdb?query=${imdb}`;
+      trakt_link.target = "_blank";
+      var img = trakt_link.appendChild(document.createElement("img"));
+      img.src = "https://tomyangsh.pw/trakt-icon.svg";
+      img.width = 30;
+    }
 
-      if (imdb) {
-        let imdb_link = social_links.appendChild(document.createElement("a"));
-        imdb_link.href = `https://www.imdb.com/title/${imdb}/`;
-        imdb_link.target = "_blank";
-        var icon = imdb_link.appendChild(document.createElement("img"));
-        icon.src = "https://tomyangsh.pw/imdb.svg";
-        icon.width = 30;
-        var trakt_link = social_links.appendChild(document.createElement("a"));
-        trakt_link.href = `https://trakt.tv/search/imdb?query=${imdb}`;
-        trakt_link.target = "_blank";
-        var img = trakt_link.appendChild(document.createElement("img"));
-        img.src = "https://tomyangsh.pw/trakt-icon.svg";
-        img.width = 30;
-      }
+    let web_date = result.web_date
 
-      let web_date = result.web_date
+    if (web_date) {
+      let web_date_span = document.querySelector('div.facts').appendChild(document.createElement("span"));
+      web_date_span.innerText = 'WEB: ' + web_date;
+    }
 
-      if (web_date) {
-        let web_date_span = document.querySelector('div.facts').appendChild(document.createElement("span"));
-        web_date_span.innerText = 'WEB: ' + web_date;
-      }
+    var castinfo = [];
+    var i;
+    for (i of result.cast) {
+     if (i.character) {
+       castinfo.push(`${i.name} 饰 ${i.character}`);
+     } else {
+       castinfo.push(i.name);
+     }
+    }
 
-      let next_episode = result.next_episode
-
-      if (next_episode) {
-        let next_episode_span = document.querySelector('div.facts').appendChild(document.createElement("span"));
-        next_episode_span.innerText = '下一集：' + next_episode;
-      }
-
-      var castinfo = [];
-      var i;
-      for (i of result.cast) {
-       if (i.character) {
-         castinfo.push(`${i.name} 饰 ${i.character}`);
-       } else {
-         castinfo.push(i.name);
-       }
-      }
-
-      var ptinfo;
-      if (result.cat == 'movie') {
-        ptinfo = `[img]${result.poster}[/img]
+    var ptinfo;
+    if (result.cat == 'movie') {
+      ptinfo = `[img]${result.poster}[/img]
 [size=3]
 [b]${result.name} ${result.ori_name} (${result.year})[/b]
 
@@ -91,8 +81,8 @@ IMDb  https://www.imdb.com/title/${result.imdb}/
 
 ${result.des}
 [/size]`
-      } else {
-        ptinfo = `[img]${result.poster_main}[/img]
+    } else {
+      ptinfo = `[img]${result.poster_main}[/img]
 [size=3]
 [b]${result.name} ${result.ori_name} (${result.year})[/b]
 
@@ -107,25 +97,22 @@ IMDb  https://www.imdb.com/title/${result.imdb}/
 
 ${result.des}
 [/size]`
-      }
-
-      var ptinfo_button = document.querySelector('h2').appendChild(document.createElement("button"))
-      ptinfo_button.innerText = '复制ptinfo'
-      ptinfo_button.setAttribute("class", "copy");
-      ptinfo_button.style = "font-size: 15px; cursor: pointer; color: black; font-weight: normal;"
-
-      unsafeWindow.ptinfo = ptinfo;
-      var clipboard = new ClipboardJS('.copy', {
-        text: function () {
-          return ptinfo;
-        },
-      });
-
-      clipboard.on('success', function () {
-        document.querySelector('button.copy').innerText = '已复制';
-      });
     }
-  }
 
-  xhr.send();
-})()
+    var ptinfo_button = document.querySelector('h2').appendChild(document.createElement("button"))
+    ptinfo_button.innerText = '复制ptinfo'
+    ptinfo_button.setAttribute("class", "copy");
+    ptinfo_button.style = "font-size: 15px; cursor: pointer; color: black; font-weight: normal;"
+
+    unsafeWindow.ptinfo = ptinfo;
+    var clipboard = new ClipboardJS('.copy', {
+      text: () => ptinfo
+    });
+
+    clipboard.on('success', function () {
+      document.querySelector('button.copy').innerText = '已复制';
+    });
+  }
+}
+
+xhr.send();
