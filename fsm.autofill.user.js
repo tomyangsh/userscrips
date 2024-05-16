5aadc6ffdfed3086b02b005d679670970f06a7e3
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2024-05-15 21:05:30 -0400

    fsm.autofill: update description

--- fsm.autofill.user.js
+++ fsm.autofill.user.js
@@ -13,9 +13,9 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM.xmlHttpRequest
-// @version     2.5
+// @version     2.5.1
 // @author      大統領
-// @description 馒头/emp/pb/ptt/exo 一键转种至 fsm
+// @description 馒头/emp/pb/ptt/exo/kamept/kufirc 一键转种至 fsm
 // @icon        https://img.fsm.name/21/69/2169f715a4805d2643db30a4b8fd95d0.jpg
 // ==/UserScript==
 
